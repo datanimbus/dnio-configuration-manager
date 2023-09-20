3736a5502d71bef88f8d7ab37108e8cfa37eec42
Jugnu Agrawal <jugnu@appveen.com>
2023-09-20 14:53:57 +0530

    dockerhub change

--- scripts/push_hub.sh
+++ scripts/push_hub.sh
@@ -5,48 +5,48 @@ set -e
 TAG=`cat CURRENT_CM`
 
 echo "****************************************************"
-echo "data.stack:cm :: Pushing Image to Docker Hub :: appveen/data.stack.cm:$TAG"
+echo "data.stack:cm :: Pushing Image to Docker Hub :: datanimbus/data.stack.cm:$TAG"
 echo "****************************************************"
 
-docker tag data.stack.cm:$TAG appveen/data.stack.cm:$TAG
-docker push appveen/data.stack.cm:$TAG
+docker tag data.stack.cm:$TAG datanimbus/data.stack.cm:$TAG
+docker push datanimbus/data.stack.cm:$TAG
 
 echo "****************************************************"
-echo "data.stack:cm :: Image Pushed to Docker Hub AS appveen/data.stack.cm:$TAG"
+echo "data.stack:cm :: Image Pushed to Docker Hub AS datanimbus/data.stack.cm:$TAG"
 echo "****************************************************"
 
 
 echo "****************************************************"
-echo "data.stack:cm :: Pushing Image to Docker Hub :: appveen/data.stack.flow.base:$TAG"
+echo "data.stack:cm :: Pushing Image to Docker Hub :: datanimbus/data.stack.flow.base:$TAG"
 echo "****************************************************"
 
-docker tag data.stack.flow.base:$TAG appveen/data.stack.flow.base:$TAG
-docker push appveen/data.stack.flow.base:$TAG
+docker tag data.stack.flow.base:$TAG datanimbus/data.stack.flow.base:$TAG
+docker push datanimbus/data.stack.flow.base:$TAG
 
 echo "****************************************************"
-echo "data.stack:cm :: Image Pushed to Docker Hub AS appveen/data.stack.flow.base:$TAG"
+echo "data.stack:cm :: Image Pushed to Docker Hub AS datanimbus/data.stack.flow.base:$TAG"
 echo "****************************************************"
 
 
 # echo "****************************************************"
-# echo "data.stack:cm :: Pushing Image to Docker Hub :: appveen/data.stack.b2b.base:$TAG"
+# echo "data.stack:cm :: Pushing Image to Docker Hub :: datanimbus/data.stack.b2b.base:$TAG"
 # echo "****************************************************"
 
-# docker tag data.stack.b2b.base:$TAG appveen/data.stack.b2b.base:$TAG
-# docker push appveen/data.stack.b2b.base:$TAG
+# docker tag data.stack.b2b.base:$TAG datanimbus/data.stack.b2b.base:$TAG
+# docker push datanimbus/data.stack.b2b.base:$TAG
 
 # echo "****************************************************"
-# echo "data.stack:bm :: Image Pushed to Docker Hub AS appveen/data.stack.b2b.base:$TAG"
+# echo "data.stack:bm :: Image Pushed to Docker Hub AS datanimbus/data.stack.b2b.base:$TAG"
 # echo "****************************************************"
 
 
 # echo "****************************************************"
-# echo "data.stack:cm :: Pushing Image to Docker Hub :: appveen/data.stack.faas.base:$TAG"
+# echo "data.stack:cm :: Pushing Image to Docker Hub :: datanimbus/data.stack.faas.base:$TAG"
 # echo "****************************************************"
 
-# docker tag data.stack.faas.base:$TAG appveen/data.stack.faas.base:$TAG
-# docker push appveen/data.stack.faas.base:$TAG
+# docker tag data.stack.faas.base:$TAG datanimbus/data.stack.faas.base:$TAG
+# docker push datanimbus/data.stack.faas.base:$TAG
 
 # echo "****************************************************"
-# echo "data.stack:bm :: Image Pushed to Docker Hub AS appveen/data.stack.faas.base:$TAG"
+# echo "data.stack:bm :: Image Pushed to Docker Hub AS datanimbus/data.stack.faas.base:$TAG"
 # echo "****************************************************"
